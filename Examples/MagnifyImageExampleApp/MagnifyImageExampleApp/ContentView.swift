8913ec947882b8db4b95797473137e90f6104af6
akihiko-sato <akihiko.sato@dena.com>
2025-04-19 19:04:24 +0900

    Fix sample project

--- Examples/MagnifyImageExampleApp/MagnifyImageExampleApp/ContentView.swift
+++ Examples/MagnifyImageExampleApp/MagnifyImageExampleApp/ContentView.swift
@@ -6,19 +6,19 @@
 //
 
 import SwiftUI
+import MagnifyImageKit
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      if let image = UIImage(named: "CherryBlossoms") {
+        MagnifyImageView(image: image)
+          .ignoresSafeArea()
+      }
     }
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
