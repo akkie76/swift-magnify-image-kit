2f133fdbbc8c190969466469270579a89cb016af
akihiko-sato <akihiko.sato@dena.com>
2025-04-19 19:03:03 +0900

    Add sample project

---
+++ Examples/MagnifyImageExampleApp/MagnifyImageExampleApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MagnifyImageExampleApp
+//
+//  Created by Akihiko Sato on 2025/04/19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Examples/MagnifyImageExampleApp/MagnifyImageExampleApp/MagnifyImageExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagnifyImageExampleAppApp.swift
+//  MagnifyImageExampleApp
+//
+//  Created by Akihiko Sato on 2025/04/19.
+//
+
+import SwiftUI
+
+@main
+struct MagnifyImageExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
