b3844c0d05f7d1eac5c143f8f09bddb91bae0b1a
akihiko-sato <akihiko.sato@dena.com>
2025-04-20 00:33:05 +0900

    Update Package

--- Package.swift
+++ Package.swift
@@ -4,21 +4,24 @@
 import PackageDescription
 
 let package = Package(
-    name: "MagnifyImageKit",
-    products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "MagnifyImageKit",
-            targets: ["MagnifyImageKit"]),
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "MagnifyImageKit"),
-        .testTarget(
-            name: "MagnifyImageKitTests",
-            dependencies: ["MagnifyImageKit"]
-        ),
-    ]
+  name: "MagnifyImageKit",
+  platforms: [
+    .iOS(.v13)
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, making them visible to other packages.
+    .library(
+      name: "MagnifyImageKit",
+      targets: ["MagnifyImageKit"]),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .target(
+      name: "MagnifyImageKit"),
+    .testTarget(
+      name: "MagnifyImageKitTests",
+      dependencies: ["MagnifyImageKit"]
+    ),
+  ]
 )
